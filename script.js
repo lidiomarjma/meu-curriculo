928353855eb1213c930aad0d68523d124eb2c228
Lidiomar JM Alves <lidiomarmillaalves@gmail.com>
2026-08-08 12:37:21 -0300

    fix: ajustes refinados.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function gerarPDF() {
   const elemento = document.getElementById('curriculo');
 
   const opcoes = {
-    margin: [8, 8, 8, 8], // Margem de 12mm em todas as bordas do papel
+    margin: 0,
     filename: 'Curriculo_Lidiomar_Alves.pdf',
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: {
@@ -10,7 +10,8 @@ function gerarPDF() {
       useCORS: true,
       logging: false,
       scrollX: 0,
-      scrollY: 0
+      scrollY: 0,
+      windowWidth: document.documentElement.offsetWidth
     },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
   };
