02875ec1fd445f87a90407ab1a2d54851ead72e1
Lidiomar JM Alves <lidiomarmillaalves@gmail.com>
2026-08-08 10:44:18 -0300

    fix: ajustes A4.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function gerarPDF() {
     margin: 0,
     filename: 'Curriculo_Lidiomar_Alves.pdf',
     image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2, useCORS: true, logging: false },
+    html2canvas: { scale: 2, useCORS: true, logging: false, windowWidth: 794 },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
   };
 
